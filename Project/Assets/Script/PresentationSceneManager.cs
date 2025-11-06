90f66cff865b831237cd2c870385f78bb106e289
Marcello Miranda <marcellomiranda2004@gmail.com>
2025-11-06 14:59:51 +0100

    aggiunta animazione

--- Project/Assets/Script/PresentationSceneManager.cs
+++ Project/Assets/Script/PresentationSceneManager.cs
@@ -1,16 +1,34 @@
+using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class PresentationSceneManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private PresentationSceneManager _instance;
+    public PresentationSceneManager Instance { get => _instance; }
+
+    [Header("Tony Slide In Reference")]
+    [SerializeField] private Animator tonySprite;
+
+    [Header("Baloon 1")]
+    [SerializeField] private Animator baloonSprite1;
+    [SerializeField] private TextMeshProUGUI dialogoText1;
+
     void Start()
     {
-        
+        StartCoroutine(TonySlideIn());
+        StartCoroutine(BaloonSlideIn());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator TonySlideIn()
     {
-        
+        tonySprite.Play("TonySlideInToBottom");
+        yield return new WaitForSeconds(1f);
+    }
+
+    IEnumerator BaloonSlideIn()
+    {
+        baloonSprite1.Play("DialogTextSlideInToUp");
+        yield return new WaitForSeconds(2f);
     }
 }
